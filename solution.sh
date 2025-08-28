381d1e28416e16d767e2262f974264055e382178
am25b023 <am25b023@smail.iitm.ac.in>
2025-08-28 12:13:22 +0530

    Update solution.sh

--- solution.sh
+++ solution.sh
@@ -2,17 +2,29 @@
                                                        Modified  
 
 #!/bin/bash
+
 sort numbers.txt
+
 ipconfig getifaddr en0
+
 cat readme.txt
+
 wc -l data.csv
-grep -l -r -i error logs/
+
+grep -lri error logs/
+
 tail -10 app.log
+
 chmod +x script.sh
+
 grep -i -r TODO *.py
+
 history 20
+
 ps aux --sort -%mem
+
 find / -type d -name "backup"
+
 sed 's/foo/bar/g' example.txt > new_example.txt
 
 
